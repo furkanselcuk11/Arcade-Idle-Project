18e114ffaf41756bfcf6234526931f4480be219b
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2022-08-20 13:43:39 +0300

    +add: PlayerController and CameraContreoller added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _chaseSpeed = 5;
+
+    private void LateUpdate()
+    {
+        Vector3 desPos = _target.position + _offset;  // Kamera ile takip edilen obje arasındaki mesafe
+        transform.position = Vector3.Lerp(transform.position, desPos, _chaseSpeed);   // Kamera pozisynu yumuþak geçiþ ile aradaki mesafe kader uzaktan takip eder
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    private Animator anim;
+    [Space]
+    [Header("Player Controller")]
+    [SerializeField] private float _movementSpeed=300;
+    [SerializeField] private float _rotationSpeed = 500;
+    private bool isRunnig;
+
+    [Space]
+    [Header("Joystick Controller")]
+    [SerializeField] Joystick joystick;   // Joystick scripti
+    float vertical, horizontal; // Player yönü  
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (isRunnig)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        JoystickMove();   // Joystick kontrolü çalýþýr 
+    }
+    public void JoystickMove()
+    {
+        // Joystick kontrolü
+        vertical = joystick.Vertical * _movementSpeed * Time.fixedDeltaTime;
+        horizontal = joystick.Horizontal * _movementSpeed * Time.fixedDeltaTime;
+
+        rb.velocity = new Vector3(horizontal, 0, vertical);
+        if (rb.velocity != Vector3.zero)
+        {
+            isRunnig = true;
+            Quaternion temp = Quaternion.LookRotation(rb.velocity, Vector3.up);
+            rb.rotation = Quaternion.RotateTowards(transform.rotation, temp, _rotationSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            isRunnig = false;
+        }
+    }
+}
