749372a732a14976100ac19c706d40c78f79156f
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2022-08-24 20:58:53 +0300

    add: Environments objects added

--- Assets/Scripts/BuyArea.cs
+++ Assets/Scripts/BuyArea.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BuyArea : MonoBehaviour
 {
     [SerializeField] private GameObject farmerAndShopObject, buyObject;
-    [SerializeField] private float cost;    // Satýn alýnacak öðenin maliyeti
-    [SerializeField] private float currentMoney; // Ödenen para miktarý
+    [SerializeField] private int cost;    // Satýn alýnacak öðenin maliyeti
+    [SerializeField] private int currentMoney; // Ödenen para miktarý
     [SerializeField] private float progress;
     [SerializeField] private Image progressImage;
+    [SerializeField] private TextMeshProUGUI areaText;
 
+    private void Start()
+    {
+        areaText.text = " $ " + (cost-currentMoney);
+    }
     public void Buy(int valueMoney)
     {
-        currentMoney += valueMoney;
-        progress = currentMoney / cost;
-        progressImage.fillAmount = progress;
-        if (progress >= 1)
+        if (currentMoney <cost)
         {
-            // Eðer satýn alma tamamlandýysa
-            buyObject.SetActive(false); // Satýn alma triggerini pasif yapar
-            farmerAndShopObject.SetActive(true);    // Satýn alýnan öðeyi aktif hale getir
-            this.enabled = false;   // Satýn alma iþlemi tamamlanýnca kodu kapat
+            currentMoney += valueMoney;
+            areaText.text = " $ " + (cost - currentMoney);
+            progress = (currentMoney / cost);
+            progressImage.fillAmount = progress;
+            if (currentMoney == cost)
+            {
+                // Eðer satýn alma tamamlandýysa
+                buyObject.SetActive(false); // Satýn alma triggerini pasif yapar
+                farmerAndShopObject.SetActive(true);    // Satýn alýnan öðeyi aktif hale getir
+                this.enabled = false;   // Satýn alma iþlemi tamamlanýnca kodu kapat
+            }
         }
+
     }
 }
